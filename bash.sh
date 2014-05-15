ed4268636a4d187f4562f44e3f40c07519d7ed83
Christian Caspers <x.caspers@gmail.com>
2014-05-15 16:19:41 +0200

    added custom prompt

--- bash.sh
+++ bash.sh
@@ -1,4 +1,10 @@
+#aliases
 alias g='git'
 alias nano='nano -c -T4'
 alias pico='nano'
 alias n='nano'
+
+#prompt
+LINE1="\n\[\e[32;1m\](\[\e[37;1m\]\u\[\e[32;1m\])-(\[\e[37;1m\]jobs:\j\[\e[32;1m\])-(\[\e[37;1m\]\w\[\e[32;1m\])"
+LINE2="\n(\[\e[37;1m\]! \!\[\e[32;1m\])-> \[\e[0m\]"
+PS1=$LINE1$LINE2
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ BASHSCRIPTS=$HOME/.bash_customization
 touch $HOME/.bashrc
 if ! cat $HOME/.bashrc | grep -q ". $BASHSCRIPTS"; then
   ln -s $CWD/bash.sh $BASHSCRIPTS
-  echo ". $BASHSCRIPTS" >> $HOME/.bashrc
+  echo -e "\n# Sourcing custom modifications \n# Visit https://github.com/ccaspers/scripts for more info\n. $BASHSCRIPTS" >> $HOME/.bashrc
   echo "Scripts added to .bashrc"
 else
   echo "Scripts already linked"
