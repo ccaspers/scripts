7f344ca19c31973d467fc1ae77ee7940d173f0f7
Christian Caspers <x.caspers@gmail.com>
2014-05-15 15:08:12 +0200

    inital script and alias setup

---
+++ bash.sh
@@ -0,0 +1,4 @@
+alias g='git'
+alias nano='nano -c -T4'
+alias pico='nano'
+alias n='nano'
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CWD=$(dirname $(readlink -f $0))
+
+# Setup bashscripts
+BASHSCRIPTS=$HOME/.bash_customization
+touch $HOME/.bashrc
+if ! cat $HOME/.bashrc | grep -q ". $BASHSCRIPTS"; then
+  ln -s $CWD/bash.sh $BASHSCRIPTS
+  echo ". $BASHSCRIPTS" >> $HOME/.bashrc
+  echo "Scripts added to .bashrc"
+else
+  echo "Scripts already linked"
+fi
+
+# Setup Gitconfig
+echo "Please enter your name: "
+read -e NAME
+echo "Please enter your email: "
+read -e EMAIL
+echo "Resetting global Git-Config"
+rm -f $HOME/.gitconfig
+cp gitconfig $HOME/.gitconfig
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+echo "Done"
