75c250d125e878f1b7458987d36f1099b9438bdc
Christian Caspers <x.caspers@gmail.com>
2014-05-16 16:41:07 +0200

    added dotfiles to script and simplified gitconfig

--- install.sh
+++ install.sh
@@ -7,19 +7,17 @@ touch $HOME/.bashrc
 if ! cat $HOME/.bashrc | grep -q ". $BASHSCRIPTS"; then
   ln -s $CWD/bash.sh $BASHSCRIPTS
   echo -e "\n# Sourcing custom modifications \n# Visit https://github.com/ccaspers/scripts for more info\n. $BASHSCRIPTS" >> $HOME/.bashrc
-  echo "Scripts added to .bashrc"
+  echo " >> Scripts added to .bashrc"
 else
-  echo "Scripts already linked"
+  echo " >> Scripts already linked"
 fi
 
-# Setup Gitconfig
-echo "Please enter your name: "
-read -e NAME
-echo "Please enter your email: "
-read -e EMAIL
-echo "Resetting global Git-Config"
-rm -f $HOME/.gitconfig
-cp gitconfig $HOME/.gitconfig
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
-echo "Done"
+# Setup symlinks for dotfiles
+for file in $CWD/dotfiles/*
+do
+	name=$(basename $file)
+	link="$HOME/.$name"
+	echo " >> linking $link to $file"
+	rm -f $link
+	ln -s $file $link
+done
