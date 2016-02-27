2267b10d7270ee12fc75c5b09be228527371d2ac
Christian Caspers <(none)>
2016-02-27 15:53:02 +0100

    added compatibility for osx

--- install.sh
+++ install.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
-CWD=$(dirname $(readlink -f $0))
+set -euo pipefail
+IFS=$'\n\t'
+CWD=$(dirname $(greadlink -f $0))
 
-# Setup bashscripts
 BASHSCRIPTS=$CWD/bash.sh
-BASHRC=$HOME/.bashrc
 
-touch $BASHRC
-if ! cat $BASHRC | grep -q "$BASHSCRIPTS"; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  CWD=$(dirname $(readlink -f $0))
+  INITFILE=$HOME/.bashrc
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  CWD=$(dirname $(greadlink -f $0))
+  INITFILE=$HOME/.profile
+else
+  echo "$OSTYPE is currently not supported"
+fi
+
+touch $INITFILE
+if ! cat $INITFILE | grep -q "$BASHSCRIPTS"; then
   # Don't replace tabs with spaces in here-doc
   cat <<-EOS >> $BASHRC
-	
-	# Sourcing custom modifications
+
+	# Sourcing personal configuration
 	# Visit https://github.com/ccaspers/scripts for more info
 	source $BASHSCRIPTS
 	EOS
-  echo ">> Scripts added to .bashrc"
+  echo ">> Scripts added to $INITFILE"
 else
   echo " >> Scripts already linked"
+  exit 0
 fi
 
 # Setup symlinks for dotfiles
 for file in $CWD/dotfiles/*
 do
-	name=$(basename $file)
-	link="$HOME/.$name"
-	echo " >> linking $link to $file"
-	rm -f $link
-	ln -s $file $link
+  name=$(basename $file)
+  link="$HOME/.$name"
+  echo " >> linking $link to $file"
+  rm -f $link
+  ln -s $file $link
 done
