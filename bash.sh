e9ea6e79b5d8e7d664c90339d39c09a1e7a66ac5
Christian Caspers <x.caspers@gmail.com>
2016-03-02 14:21:21 +0100

    add convenient aliases for ls

--- bash.sh
+++ bash.sh
@@ -9,6 +9,14 @@ alias n='nano'
 alias agu='sudo apt-get update && sudo apt-get upgrade'
 alias agi='sudo apt-get install'
 
+unalias l > /dev/null 2>&1
+unalias la > /dev/null 2>&1
+unalias ll > /dev/null 2>&1
+alias l='ls'
+alias la='ls -a'
+alias ll='ls -l'
+
+
 #prompt
 LINE1="\n\[\e[32;1m\](\[\e[37;1m\]\u\[\e[32;1m\])-(\[\e[37;1m\]jobs:\j\[\e[32;1m\])-(\[\e[37;1m\]\w\[\e[32;1m\])"
 LINE2="\n(\[\e[37;1m\]! \!\[\e[32;1m\])-> \[\e[0m\]"
@@ -21,3 +29,6 @@ export -f fork
 #add .local/bin to path
 export PATH=$PATH:~/.local/bin
 
+# grep
+alias grep='grep --color=auto'
+export GREP_COLOR='1;35;40'
