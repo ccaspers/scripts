106fc4442aff65bc4d3100c8d2bfac96d837e47b
Christian Caspers <(none)>
2016-07-15 12:53:11 +0200

    fix installer

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-CWD=$(dirname $(greadlink -f $0))
-
-BASHSCRIPTS=$CWD/bash.sh
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   CWD=$(dirname $(readlink -f $0))
@@ -13,12 +10,15 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   INITFILE=$HOME/.profile
 else
   echo "$OSTYPE is currently not supported"
+  exit 1
 fi
+
+BASHSCRIPTS=$CWD/bash.sh
 
 touch $INITFILE
 if ! cat $INITFILE | grep -q "$BASHSCRIPTS"; then
   # Don't replace tabs with spaces in here-doc
-  cat <<-EOS >> $BASHRC
+  cat <<-EOS >> $INITFILE
 
 	# Sourcing personal configuration
 	# Visit https://github.com/ccaspers/scripts for more info
